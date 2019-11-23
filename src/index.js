1e7e553f277b47c7856f4452fbce9579f4662036
joshuaagapay <jpagapay94@gmail.com>
2019-11-23 19:34:20 +0800

    Add materialize-css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import '../node_modules//materialize-css/dist/css/materialize.min.css';
+import '../node_modules//materialize-css/dist/js/materialize.min.js';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
