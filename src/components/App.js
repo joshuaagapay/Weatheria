9a80a4c1018f1fa59fe1d9ff53a88a1282fefa80
joshuaagapay <jpagapay94@gmail.com>
2019-11-25 23:14:25 +0800

    add react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <p>Hello React</p>
+        <p><h1>Hello React</h1></p>
       </div>
     )
   }
