b10f9b4b4a07e7aae329d5b8bc9262243c047493
joshuaagapay <jpagapay94@gmail.com>
2019-12-20 23:00:20 +0800

    add WeatherDetails Component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from '../assets/unknown.png'
 import Notification from './Notification'
+import WeatherDetails from './WeatherDetails'
 
 class Weather extends React.Component {
 
@@ -9,10 +9,7 @@ class Weather extends React.Component {
       <div className="weather container">
         <div className="weather-name"><h2><p>Weatheria</p></h2></div>
         <Notification />
-        <div className="weather-icon"><img src={logo} /></div>
-        <div className="temperature-value"><h2><p>-°<span>C</span></p></h2></div>
-        <div className="temperature-description"><p>-</p></div>
-        <div className="location"><p>-</p></div>
+        <WeatherDetails />
       </div>
     )
   }
---
+++ src/components/WeatherDetails.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../assets/unknown.png'
+
+class WeatherDetails extends React.Component {
+
+  render() {
+    return (
+      <div classNmae="weather-details">
+        <div className="weather-icon"><img src={logo} /></div>
+        <div className="temperature-value"><h2><p>-°<span>C</span></p></h2></div>
+        <div className="temperature-description"><p>-</p></div>
+        <div className="location"><p>-</p></div>
+      </div>
+    )
+  }
+}
+
+export default WeatherDetails
