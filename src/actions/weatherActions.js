3095310ed532634827481ca0f74b107f45716656
joshuaagapay <jpagapay94@gmail.com>
2019-12-28 02:32:25 +0800

    update actions

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,13 +1,38 @@
-import { GET_WEATHER } from './types'
+import { GET_WEATHER, GET_LOCATION_FAILED, GET_LOCATION_SUCCESS } from './types'
+
 import axios from 'axios'
 const api_key = process.env.API_KEY;
 
 export const getWeather = (latitude, longitude) => {
   const api = `https:\\api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
   return (dispatch) => {
-    axios.get(api).then((response) =>{
-      dispatch({type: 'GET_WEATHER', result:response});
+    axios.get(api).then((response) => {
+      dispatch({ type: 'GET_WEATHER', result: response });
     });
   }
 
 }
+
+export const getLocation = () => {
+
+  return (dispatch) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+       if(position) {
+        dispatch({ type: GET_LOCATION_SUCCESS, position: position });
+       } 
+      }, error => {
+        if(error.code === 1){
+          dispatch({ type: GET_LOCATION_FAILED, error: "User denied the request for Geolocation." });
+        }
+      });
+    }
+  }
+}
+
+// export const showPosition = (position) => {
+//   return (dispatch) => {
+//     dispatch({ type: GET_LOCATION_SUCCESS, position: position });
+//   }
+// }
+
